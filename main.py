e86a294654fbe172eb14b66a401c245d72b690c5
Max <linme00p@gmail.com>
2024-01-02 19:45:01 -0500

    Clean up main and play with sending files.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import logging
 from dotenv import load_dotenv
 import os
+import asyncio
+import requests
 
-from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram.ext import Application
 
 load_dotenv()
 
@@ -17,42 +18,23 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 
-# Define a few command handlers. These usually take the two arguments update and
-# context.
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /start is issued."""
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
-    )
-
-
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /help is issued."""
-    await update.message.reply_text("Help!")
-
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
-    await update.message.reply_text(update.message.text)
-
-
-def main() -> None:
+async def main() -> None:
     """Start the bot."""
-    # Create the Application and pass it your bot's token.
     application = Application.builder().token(os.getenv("TOKEN")).build()
+    bot = application.bot
+    chat_id = os.getenv("CHAT_ID")
 
-    # on different commands - answer in Telegram
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
+    # print(await application.bot.send_document(
+    #     chat_id,
+    #     open("test.txt", "rb"),
+    # ))
 
-    # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
-    # Run the bot until the user presses Ctrl-C
-    application.run_polling(allowed_updates=Update.ALL_TYPES)
-
+    # f = await application.bot.get_file(file_id)
+    # link = f.file_path
+    # with open("test2.txt", "wb") as file:
+    #     file.write(requests.get(link).content)
+    
+    # await bot.delete_message(chat_id, message_id)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
