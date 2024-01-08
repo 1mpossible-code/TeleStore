f393c8ad02eb940ee973a195da2767709d367689
Max <linme00p@gmail.com>
2024-01-07 22:48:05 -0500

    Comment App.py and rename method save to upload.

--- App.py
+++ App.py
@@ -11,15 +11,50 @@ MB_TO_BYTES = 1024 * 1024
 
 
 class App:
-    def __init__(self, token, chat_id, db_name, temp_dir, files_dir) -> None:
+    """
+    The main application class that handles the bot, database, and file management components.
+
+    Attributes:
+        bot (Bot): An instance of the Bot class for interacting with the Telegram bot API.
+        files_db (Files): An instance of the Files class for managing the database of files.
+        file_manager (FileManager): An instance of the FileManager class for handling file operations.
+        cryptography (Cryptography): An instance of the Cryptography class for file encryption and decryption.
+
+    Methods:
+        upload_file: Saves a file by either sending it directly or handling it as a large file.
+        download_file: Downloads a file based on its unique identifier.
+        get_all_files_info: Gets information about all files.
+        delete_file: Deletes a file based on its unique identifier.
+        __init__: Initializes the application with bot, database, and file management components.
+        _handle_large_file: Handles a large file by splitting and sending it in parts.
+        _send_multiple_files: Sends multiple files and returns message and file IDs.
+        _send_file: Sends a single file.
+    """
+    def __init__(self) -> None:
         """Initialize the application with bot, database, and file management components."""
+        # We get the token and chat ID from environment variables.
+        token = os.getenv("TOKEN")
+        chat_id = os.getenv("CHAT_ID")
+
+        # Now we optionally get the database name, temp directory, and files directory from environment variables.
+        # If they are not set, we use the default values.
+        db_name = os.getenv("DB_NAME") or "db.sqlite"
+        temp_dir = os.getenv("TEMP_DIR") or "temp"
+        files_dir = os.getenv("FILES_DIR") or "files"
+        
+        # Create an instance of the bot, database, file manager, and cryptography classes.
         self.bot = Bot(token, chat_id)
         self.files_db = Files(db_name)
         self.file_manager = FileManager(temp_dir, files_dir)
         self.cryptography = Cryptography()
 
-    async def save_file(self, file_path: str) -> None:
-        """Save a file by either sending it directly or handling it as a large file."""
+    async def upload_file(self, file_path: str) -> None:
+        """Uploads the file by sending it directly to the bot or handling it as a large file.
+        Large files are split into parts and sent in parts.
+
+        Args:
+            file_path (str): The path of the file to be saved.
+        """
         if self.file_manager.is_file_large(file_path, MAX_FILE_SIZE_MB * MB_TO_BYTES):
             logging.info(f"Handling large file: {file_path}")
             await self._handle_large_file(file_path)
@@ -28,7 +63,11 @@ class App:
             await self._send_file(file_path)
 
     async def _handle_large_file(self, file_path: str):
-        """Handle a large file by splitting and sending it in parts."""
+        """Handle a large file by splitting and sending it in parts.
+
+        Args:
+            file_path (str): The path of the large file to be handled.
+        """
         size = os.path.getsize(file_path)
         logging.info("Splitting the large file...")
         split_files = self.file_manager.split_file(
@@ -42,7 +81,14 @@ class App:
         self.file_manager.clean_directory()
 
     async def _send_multiple_files(self, file_paths):
-        """Send multiple files and return message and file IDs."""
+        """Send multiple files and return message and file IDs.
+
+        Args:
+            file_paths (list): List of file paths to be sent.
+
+        Returns:
+            tuple: A tuple containing the message IDs and file IDs.
+        """
         msg_ids = []
         file_ids = []
         for file in file_paths:
@@ -55,7 +101,11 @@ class App:
         return msg_ids, file_ids
 
     async def _send_file(self, file_path: str):
-        """Send a single file."""
+        """Send a single file.
+
+        Args:
+            file_path (str): The path of the file to be sent.
+        """
         size = os.path.getsize(file_path)
         logging.info(f"Encrypting file: {file_path}")
         self.cryptography.encrypt_file(file_path)
@@ -66,7 +116,12 @@ class App:
         logging.info("File sent and recorded in the database.")
 
     async def download_file(self, uid: int) -> None:
-        """Download a file based on its unique identifier."""
+        """Download a file based on its unique identifier. This downloads the file from the bot storage and decrypts it.
+        If the file is large, it is downloaded in parts and then joined.
+
+        Args:
+            uid (int): The unique identifier of the file to be downloaded.
+        """
         logging.info(f"Downloading file with UID: {uid}")
         file_info = self.files_db.get_file(uid)
         if not file_info:
@@ -92,7 +147,12 @@ class App:
             
             # join file parts
             logging.info("Joining file parts...")
-            os.system(f"cat {self.file_manager.temp_dir}/{file_info[1]}/* > {file_path}")
+            # if windows, use copy /b command
+            if (os.name == "nt"):
+                os.system(f"copy /b {self.file_manager.temp_dir}/{file_info[1]}/* {file_path}")
+            else:
+                # if linux, use cat command
+                os.system(f"cat {self.file_manager.temp_dir}/{file_info[1]}/* > {file_path}")
             logging.info("File parts joined.")
 
         logging.info(f"File downloaded: {file_path}")
@@ -102,12 +162,26 @@ class App:
         os.rmdir(f"{self.file_manager.temp_dir}/{file_info[1]}/")
 
     async def get_all_files_info(self) -> list:
-        """Get information about all files."""
+        """Get information about all files from the database.
+
+        Returns:
+            list: A list of file information. Each file is represented as a tuple containing the following information:
+                id (int): The ID of the file.
+                name (str): The name of the file.
+                message_ids (str): The IDs of the messages associated with the file.
+                file_ids (str): The IDs of the files associated with the file.
+                size (int): The size of the file in bytes.
+        """
         logging.info("Fetching information for all files.")
         return self.files_db.get_all_files()
 
     async def delete_file(self, uid: int) -> None:
-        """Delete a file based on its unique identifier."""
+        """Delete a file based on its unique identifier. This deletes the file from the database and bot storage.
+        It finds all messages associated with the file and deletes them.
+
+        Args:
+            uid (int): The unique identifier of the file to be deleted.
+        """
         logging.info(f"Deleting file with UID: {uid}")
         file_info = self.files_db.get_file(uid)
         if not file_info:
