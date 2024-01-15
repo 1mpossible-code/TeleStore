de9eb1f48b6524308adf1913051b0baf40cd5c6a
george sirichartchai <109933928+gsiri-code@users.noreply.github.com>
2024-01-15 18:52:40 -0500

    Condense read, download, and delete functionality with snake case naming

--- api.py
+++ api.py
@@ -31,51 +31,36 @@ def handle_Post():
             bot.file_manager.clean_directory()
             return jsonify({"message": "File successfully uploaded"}), 200    
 
-#  READ 
 @app.route('/uploads/', methods=['GET'])
-@app.route('/uploads/<int:Uid>',methods=['GET'])
-def handle_GetbyID(Uid=None):
+@app.route('/uploads/<int:uid>', methods=['GET', 'DELETE'])
+def handle_uploads(uid=None):
+
     if request.method == 'GET':
+        download = request.args.get('download', default=False, type=bool)
+        
         try:
-            if Uid: #if Uid is specified show one
-                data_tup = asyncio.run(bot.get_file_info(Uid))
-                return jsonify({'id': data_tup[0], 'name': data_tup[1], 'message_ids':data_tup[2],'file_ids':data_tup[3],'size':data_tup[4],})
-            else: # If no Uid is specified show all
-                data = asyncio.run(bot.get_all_files_info())
-                data_dict = [{'id': item[0], 'name': item[1], 'message_ids':item[2],'file_ids':item[3],'size':item[4],} for item in data]
+            if uid:
+                file_info = asyncio.run(bot.get_file_info(uid))
+
+                if download:
+                    asyncio.run(bot.download_file(uid))
+                    bot.file_manager.clean_files_directory()
+                    return send_from_directory(bot.file_manager.files_dir, file_info[1], as_attachment=True)
+                
+                return jsonify({'id': file_info[0], 'name': file_info[1], 'message_ids': file_info[2], 'file_ids': file_info[3], 'size': file_info[4]})
+            else:
+                all_files_info = asyncio.run(bot.get_all_files_info())
+                data_dict = [{'id': item[0], 'name': item[1], 'message_ids': item[2], 'file_ids': item[3], 'size': item[4]} for item in all_files_info]
                 return jsonify(data_dict)
         except ValueError:
-            return jsonify({'error':f'Upload {Uid} not found',
-                        'message': 'invalid resource URI'}),404
-        
-#DELETE 
-@app.route('/uploads/<int:Uid>',methods=['DELETE'])
-def handle_Delete(Uid):
+            return jsonify({'error': f'Upload {uid} not found', 'message': 'invalid resource URI'}), 404
+
     if request.method == 'DELETE':
         try:
-            asyncio.run(bot.delete_file(Uid))
-            return jsonify({"message": f"File {Uid} successfully deleted!"}), 200    
+            asyncio.run(bot.delete_file(uid))
+            return jsonify({"message": f"File {uid} successfully deleted!"}), 200
         except ValueError:
-            return jsonify({'error': f'Upload {Uid} not found',
-                        'message': 'invalid resource URI'}),404
-
-#DOWNLOAD
-@app.route('/download/<int:Uid>',methods=['GET'])
-def handle_Download(Uid):
-    if request.method == 'GET':
-        try:
-            file_info = asyncio.run(bot.get_file_info(Uid))
-            
-            asyncio.run(bot.download_file(Uid))
-            send_from_directory( bot.file_manager.files_dir,file_info[1],as_attachment=True)
-            
-            bot.file_manager.clean_files_directory()
-            return jsonify({"message": f"File {Uid} successfully downloaded!"}), 200    
-        except ValueError:
-            return jsonify({'error': f'Upload {Uid} not found',
-                        'message': 'invalid resource URI'}),404
-
-
+            return jsonify({'error': f'Upload {uid} not found', 'message': 'invalid resource URI'}), 404
 
 if __name__ == "__main__":
     asyncio.run(app.run(debug=True, port=3000))
