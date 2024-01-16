31d8e7a299f471015fc47c2828ee3e46b4ddf994
iminthecoupwiththecutta <george.siri2004@gmail.com>
2024-01-15 23:22:51 -0500

    Implemented Read, Delete, and Download functionality with api controller

--- UploadsController.py
+++ UploadsController.py
@@ -1,17 +1,20 @@
 
 from flask import  request, jsonify,send_from_directory
 import asyncio
+from App import App
 import os
 
 class UploadsController:
-    def __init__(self, upload_bot):
-        self.bot = upload_bot
+    def __init__(self,):
+        self.bot = App()
+        
         
     def handle_download(self,uid,file_info):
         asyncio.run(self.bot.download_file(uid))
         res = send_from_directory(self.bot.file_manager.files_dir, file_info[1], as_attachment=True)
         self.bot.file_manager.clean_files_directory()
         return res
+    
     
     def handle_read(self,uid=None):    
         if request.method == 'GET':
@@ -21,8 +24,7 @@ class UploadsController:
                     file_info = self.bot.get_file_info(uid)
 
                     if download:
-                        self.handle_download(uid,file_info)
-                       
+                        return self.handle_download(uid,file_info)
                     return jsonify({'id': file_info[0], 'name': file_info[1], 'message_ids': file_info[2], 'file_ids': file_info[3], 'size': file_info[4]})
                 else:
                     all_files_info = asyncio.run(self.bot.get_all_files_info())
@@ -36,12 +38,13 @@ class UploadsController:
             return jsonify({"message": "No file part in the request"}), 400
         
         user_file = request.files['file']
-
+        
         if user_file.filename == '':
             return jsonify({"message": "No selected file"}), 400
         else:
-            user_file.save(os.path.join(self.bot.file_manager.temp_dir, user_file.filename))
-            asyncio.run(self.bot.upload_file(os.path.join(self.bot.file_manager.temp_dir, user_file.filename)))
+            user_file_path = os.path.join(self.bot.file_manager.temp_dir, user_file.filename)
+            user_file.save(user_file_path)
+            asyncio.run(self.bot.upload_file(user_file.filename))
             self.bot.file_manager.clean_temp_directory()
             return jsonify({"message": "File successfully uploaded"}), 200  
     
--- api.py
+++ api.py
@@ -1,14 +1,14 @@
-from flask import Flask, request, jsonify,render_template,send_from_directory
+from flask import Flask, request,render_template
 from dotenv import load_dotenv
-from App import App
+from UploadsController import UploadsController
 import asyncio
-import os
+
 
 load_dotenv()
 
 app = Flask(__name__)
-bot = App()
 
+bot = UploadsController()
 
 @app.route('/')
 def home():
@@ -19,48 +19,13 @@ def home():
 def handle_uploads(uid=None):
 
     if request.method == 'GET':
-        download = request.args.get('download')
+        return bot.handle_read(uid)
         
-        try:
-            if uid:
-                file_info = bot.get_file_info(uid)
-
-                if download:
-                    asyncio.run(bot.download_file(uid))
-                    res = send_from_directory(bot.file_manager.files_dir, file_info[1], as_attachment=True)
-                    bot.file_manager.clean_files_directory()
-                    return res
-                
-                return jsonify({'id': file_info[0], 'name': file_info[1], 'message_ids': file_info[2], 'file_ids': file_info[3], 'size': file_info[4]})
-            else:
-                all_files_info = asyncio.run(bot.get_all_files_info())
-                data_dict = [{'id': item[0], 'name': item[1], 'message_ids': item[2], 'file_ids': item[3], 'size': item[4]} for item in all_files_info]
-                return jsonify(data_dict)
-        except ValueError:
-            return jsonify({'error': f'Upload {uid} not found', 'message': 'invalid resource URI'}), 404
     elif request.method == 'POST':
-        if 'file' not in request.files:
-            return jsonify({"message": "No file part in the request"}), 400
+        return bot.handle_create()
         
-        user_file = request.files['file']
-
-        if user_file.filename == '':
-            return jsonify({"message": "No selected file"}), 400
-        else:
-            user_file.save(os.path.join(bot.file_manager.temp_dir, user_file.filename))
-            asyncio.run(bot.upload_file(os.path.join(bot.file_manager.temp_dir, user_file.filename)))
-            bot.file_manager.clean_temp_directory()
-            return jsonify({"message": "File successfully uploaded"}), 200  
-    
     elif request.method == 'DELETE':
-        try:
-            asyncio.run(bot.delete_file(uid))
-            return jsonify({"message": f"File {uid} successfully deleted!"}), 200
-        except ValueError:
-            return jsonify({'error': f'Upload {uid} not found', 'message': 'invalid resource URI'}), 404
-
-
-
+        return bot.handle_delete(uid)
 
 if __name__ == "__main__":
     asyncio.run(app.run(debug=True, port=3000))
