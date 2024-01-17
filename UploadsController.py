3a1ac3ed5cd01b676357b6186dbd37b2bc7b42b0
iminthecoupwiththecutta <george.siri2004@gmail.com>
2024-01-17 15:05:20 -0500

    Adjusted comments of UploadsController.py

--- UploadsController.py
+++ UploadsController.py
@@ -11,20 +11,25 @@ from App import App
 
 class UploadsController:
     def __init__(self):
-        '''Initialize the App instance to access its functionalities'''
+        """
+        Initializes the UploadsController with an instance of the App class.
+        This setup allows access to the App's functionalities such as file management.
+        """
         self.app = App()
 
-    def __download(self, uid: int, file_info):
-        '''
-        Handles file download: fetches the file asynchronously and sends it to the client
+    def __download(self, uid: int, file_info: tuple):
+        """
+        Handles the downloading of a file. It fetches the file identified by the uid
+        asynchronously and then sends it to the client as an attachment.
 
         Args:
-            uid (int): File id.
-            file_info (tuple): Tuple containing file attributes.
+            uid (int): Unique identifier of the file to be downloaded.
+            file_info (tuple): Tuple containing details of the file, like name and size.
 
         Returns:
-            res (file): downloaded file uid
-        '''
+            werkzeug.wrappers.Response: A Flask response object that represents 
+            the downloaded file, triggering the file download on the client's side.
+        """
 
         asyncio.run(self.app.download_file(uid))
         res = send_from_directory(
@@ -33,48 +38,60 @@ class UploadsController:
         return res
 
     def __read_all(self):
-        '''
-        Retrieves info for all files and returns it in JSON format
+        """
+        Retrieves information for all available files in JSON format, suitable for API responses.
 
         Returns:
-            uploads_dict (JSON): 
-        '''
+            flask.Response: JSON response containing details of all files.
+        """
 
         all_files_info = asyncio.run(self.app.get_all_files_info())
         uploads_dict = [{'id': item[0], 'name': item[1], 'message_ids': item[2],
                          'file_ids': item[3], 'size': item[4]} for item in all_files_info]
         return jsonify(uploads_dict)
 
-    def __read_one(self, file_info):
-        '''
-        Formats a single file's info into JSON
+    def __read_one(self, file_info: tuple):
+        """
+        Formats information of a single file into JSON.
 
-        Arg: 
-            file_info (tuple): Tuple containing file attributes.
+        Args:
+            file_info (tuple): Tuple containing details of the file, like name and size.
 
         Returns:
-            upload_dict (JSON): A dictionary object containing a specific
-            file's attributes
-        '''
+            flask.Response: JSON response containing details of the specific file.
+        """
         upload_dict = {'id': file_info[0], 'name': file_info[1],
                        'message_ids': file_info[2], 'file_ids': file_info[3],
                        'size': file_info[4]}
         return jsonify(upload_dict)
 
     def check_arg(self, arg: int) -> bool:
+        """
+        Validates if the provided argument is equal to 1. This method is primarily
+        used for parsing and validating query parameters.
+
+        Args:
+            arg (int): The argument to be validated.
+
+        Returns:
+            bool: True if arg is 1, False otherwise.
+        """
         return int(arg) == 1
 
     def read(self, uid: int = None):
-        '''
-        Handles all requests associated with GET such as, read file(s), and download
+        """
+        Handles GET requests to read file information. If a UID is provided, 
+        it returns information for the specified file; otherwise, it returns 
+        information for all files. Additionally, handles file download requests.
 
-        Arg: 
-            uid (int): File id.            
+        Args:
+            uid (int, optional): Unique identifier of the file. Defaults to None.
 
         Returns:
-            upload_dict (JSON): A dictionary object containing a specific
-            file's attributes
-        '''
+            flask.Response: JSON response containing file information or 
+            a response triggering a file download. Returns an error response
+            if the file is not found.
+        """
 
         try:
             if uid:
@@ -87,8 +104,13 @@ class UploadsController:
             return jsonify({'error': f'Upload {uid} not found', 'message': 'invalid resource URI'}), 404
 
     def create(self):
-        '''Handles uploading files '''
+        """
+        Handles the file upload process. Validates the presence of a file in the request
+        and saves the uploaded file using the App's file management capabilities.
 
+        Returns:
+            flask.Response: JSON response indicating the success or failure of the file upload.
+        """
         if 'file' not in request.files:
             return jsonify({"message": "No file part in the request"}), 400
 
@@ -105,8 +127,10 @@ class UploadsController:
         self.app.file_manager.clean_temp_directory()
         return jsonify({"message": "File successfully uploaded"}), 200
 
-    def delete(self, uid):
-        '''Handles the deletion of a file, returning a confirmation response'''
+    def delete(self, uid: int):
+        """
+        Handles the deletion of a file specified uid
+        """
         try:
             asyncio.run(self.app.delete_file(uid))
             return jsonify({"message": f"File {uid} successfully deleted!"}), 200
