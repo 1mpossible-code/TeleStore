ff69dd1af6d3984a6fb7c0e17012ce46694da4d6
iminthecoupwiththecutta <george.siri2004@gmail.com>
2024-01-18 20:09:15 -0500

    Added flask cors to api.py

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
-from flask import Flask, request,render_template
+from flask import Flask, request, render_template
 from dotenv import load_dotenv
 from UploadsController import UploadsController
+from flask_cors import CORS
 import asyncio
 
 
@@ -8,24 +9,28 @@ load_dotenv()
 
 app = Flask(__name__)
 
+CORS(app)
+
+
 @app.route('/')
 def home():
     return render_template('home.html')
-    
-@app.route('/uploads/', methods=['GET','POST'])
+
+
+@app.route('/uploads/', methods=['GET', 'POST'])
 @app.route('/uploads/<int:uid>', methods=['GET', 'DELETE'])
 def handle_uploads(uid=None):
     bot = UploadsController()
 
     if request.method == 'GET':
         return bot.read(uid)
-        
+
     elif request.method == 'POST':
         return bot.create()
-        
+
     elif request.method == 'DELETE':
         return bot.delete(uid)
 
+
 if __name__ == "__main__":
     asyncio.run(app.run(debug=True, port=3000))
-    
