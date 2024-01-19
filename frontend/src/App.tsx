b6d48883b16c577bf0fa7d11bf85dce0f0ede850
iminthecoupwiththecutta <george.siri2004@gmail.com>
2024-01-18 20:12:59 -0500

    Implemented Axios request to api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,34 @@
 import Header from './components/Header/Header';
 import MainContent from './components/MainContent/MainContent';
 import ThemeProvider from '@/components/ThemeProvider';
+import { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
-  return (
+  const [uploads, setUploads] = useState<Array<Uploads>>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  interface Uploads {
+    id: number;
+    name: string;
+    size: number;
+  }
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      const res = await axios.get<Array<Uploads>>(
+        'http://127.0.0.1:3000/uploads/'
+      );
+      setUploads(res.data);
+      setLoading(false);
+    })();
+  }, []);
+
+  return loading ? (
+    'loaf'
+  ) : (
     <div>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <Header />
