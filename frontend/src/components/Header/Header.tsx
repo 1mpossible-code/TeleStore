49749260f23cfc87558a7495ff60e4dbbbe078ad
123R3N321 <jr5887@nyu.edu>
2024-01-20 21:30:39 -0500

    Create basic Instruction component. Add Instruction in Header.

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,11 +1,29 @@
+// import {useState} from "react"
+import Instruction from "./Instruction.tsx"
+
 const Header = () => {
-  return (
-    <div className="text-center mt-14 mb-4">
-      <h1 className="font-semibold text-4xl	mb-3">TeleStore</h1>
-      <p className="text-xs opacity-50">
-        Expand Your Reach, Not Your Costs - Agile Storage for All
-      </p>
-    </div>
-  );
+    // const [switchOn, setSwitchOn] = useState<boolean>(false);
+    //
+    // const toggleInstruction = () => {
+    //   setSwitchOn((switchOn) => !switchOn);
+    // };
+
+
+    return (
+        <>
+            {/*below is the header*/}
+            <div className="text-center mt-14 mb-4">
+                <h1 className="font-semibold text-6xl	mb-3">TeleStore</h1>
+                <p className="text-s opacity-50">
+                    Expand Your Reach, Not Your Costs - Agile Storage for All
+                </p>
+            </div>
+
+            <div className="justify-center">
+                <Instruction/>
+
+            </div>
+        </>
+    );
 };
 export default Header;
