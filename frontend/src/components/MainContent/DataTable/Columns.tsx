cf94a7069e91782aa01a3d3febf109b679bea65b
iminthecoupwiththecutta <george.siri2004@gmail.com>
2024-01-21 19:31:25 -0500

    Removed Extra imports in columns

--- frontend/src/components/MainContent/DataTable/Columns.tsx
+++ frontend/src/components/MainContent/DataTable/Columns.tsx
@@ -1,28 +1,16 @@
 'use client';
-import * as React from 'react';
 import {
   CaretSortIcon,
-  ChevronDownIcon,
   DotsHorizontalIcon,
 } from '@radix-ui/react-icons';
 import {
   ColumnDef,
-  ColumnFiltersState,
-  SortingState,
-  VisibilityState,
-  flexRender,
-  getCoreRowModel,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  getSortedRowModel,
-  useReactTable,
 } from '@tanstack/react-table';
 
 import { Button } from '@/components/ui/button';
 import { Checkbox } from '@/components/ui/checkbox';
 import {
   DropdownMenu,
-  DropdownMenuCheckboxItem,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
