20e7181380ffc864bd4dc5b2c8ddc9dd51e9930b
iminthecoupwiththecutta <george.siri2004@gmail.com>
2024-01-21 22:23:53 -0500

    Setup Redux, and added MainComponent Reducer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,39 +1,19 @@
 import Header from './components/Header/Header';
 import MainContent from './components/MainContent/MainContent';
 import ThemeProvider from '@/components/ThemeProvider';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
 import './App.css';
 
-export interface Upload {
-  id: number;
-  name: string;
-  size: number;
-}
+
 
 const App = () => {
-  const [loading, setLoading] = useState<boolean>(false);
-  const [uploads, setUploads] = useState<Array<Upload>>([]);
-
-  useEffect(() => {
-    (async () => {
-      setLoading(true);
-      const res = await axios.get<Array<Upload>>(
-        'http://127.0.0.1:3000/uploads/'
-      );
-      setLoading(false);
-      setUploads(res.data);
-    })();
-  }, []);
-  return loading ? (
-    'loaf'
-  ) : (
+  
+  return (
     <div>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <Header />
-        <MainContent uploads_data={uploads} />
+        <MainContent/>
       </ThemeProvider>
-    </div>
-  );
+    </div>)
+  
 };
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import { store } from './state/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <Provider store={store}>  
     <App />
-  </React.StrictMode>,
+  </Provider>  
+
 )
