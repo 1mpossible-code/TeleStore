14116340bd76ecc335aa110ff48f2a03da14557e
sirichartchai <gs3802@nyu.edu>
2024-02-18 23:28:41 -0500

    Refacotred and Added Status state to MainContentSlice

--- frontend/src/state/mainContent/mainContentSlice.ts
+++ frontend/src/state/mainContent/mainContentSlice.ts
@@ -21,19 +21,19 @@ interface downloadArguments {
 
 
 interface mainContentState {
-  loading: boolean;
+  Fetching: boolean;
   message?:string;
   data: Upload[]; // Array of Upload objects
   error: ApiError | null; // Error state can be an ApiError object or null if there's no error
-  open:boolean;
+  status: 'idle' | 'loading' | 'succeeded' | 'failed';
 
 }
 
 const initialState: mainContentState = {
-  loading: true,
+  Fetching: true,
   data: [], // Initialize as an empty array
   error:null,
-  open:false,
+  status: 'idle',
 };
 
 
@@ -42,36 +42,33 @@ const mainContentSlice = createSlice({
   name: 'mainContent',
   initialState,
   reducers: {
-    isLoading: (state) => {
-      state.loading = true;
+    isFetching: (state) => {
+      state.Fetching = true;
     },
-    notLoading: (state) => {
-      state.loading = false;
-    },
-    isOpen: (state) => {
-      state.open = true;
-    },
-    notOpen: (state) => {
-      state.open = false;
+    notFetching: (state) => {
+      state.Fetching = false;
     },
   },
   extraReducers: (builder) => {
     builder
       .addCase(getAsync.pending, (state) => {
         console.log('getAsync is pending...');
-        state.loading = true;
+        state.Fetching = true;
+        state.status = 'loading';
       })
       .addCase(getAsync.fulfilled, (state, action: PayloadAction<Upload[]>) => {
         console.log('getAsync is fulfilled!');
-        state.loading = false;
+        state.Fetching = false;
         state.data = action.payload;
+        state.status = 'succeeded';
       })
       .addCase(getAsync.rejected, (state, action) => {
         console.log('getAsync is rejected');
-        state.loading = false;
+        state.Fetching = false;
         state.error = {
           message: action.error.message || 'Unknown error occurred',
         };
+        state.status = 'failed';
       })
       .addCase(deleteAsync.pending, () => {
         console.log('deleteAsync is pending...');
@@ -82,21 +79,26 @@ const mainContentSlice = createSlice({
         state.data = state.data.filter(
           (upload) => upload.id !== action.meta.arg
         );
+        state.status = 'succeeded';
       })
       .addCase(uploadAsync.pending, (state) => {
         console.log('uploadAsync is pending...');
-        state.loading = true;
+        state.Fetching = true;
+        state.status = 'loading';
       })
       .addCase(uploadAsync.fulfilled, (state, action) => {
         console.log('uploadAsync is fulfilled...');
-        state.loading = false;
+        state.Fetching = false;
         state.data = [...state.data, action.payload];
+        state.status = 'succeeded';
       })
-      .addCase(downloadAsync.pending, () => {
+      .addCase(downloadAsync.pending, (state) => {
         console.log('downloadAsync is pending...');
+        state.status = 'loading';
       })
-      .addCase(downloadAsync.fulfilled, () => {
+      .addCase(downloadAsync.fulfilled, (state) => {
         console.log('download is fulfilled...');
+        state.status = 'succeeded';
       });
   },
 });
@@ -144,7 +146,7 @@ export const uploadAsync = createAsyncThunk(
 
 
 
-export const { isLoading,notLoading,isOpen,notOpen } =
+export const { isFetching,notFetching, } =
   mainContentSlice.actions;
 
 export default mainContentSlice.reducer;
