94247b82d07f769ed79d801ebb16409471d5ab73
sirichartchai <gs3802@nyu.edu>
2024-03-17 17:54:20 -0400

    decorated Title

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,10 +1,10 @@
-
 const Header = () => {
 
     return (
         <>
             <div className="text-center mt-14 mb-4">
-                <h1 className="font-semibold text-6xl	mb-3">TeleStore</h1>
+                <h1 className="font-semibold text-6xl flex justify-center mb-3">Tele <p className={'text-blue-500'}><u className={'underline underline-offset-8'}>Store</u></p></h1>
+
                 <p className="text-s opacity-50">
                     Expand Your Reach, Not Your Costs - Agile Storage for All
                 </p>
