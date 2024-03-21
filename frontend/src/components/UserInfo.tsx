6168713be99a79b67a3104c7196297cc0acbbddf
sirichartchai <gs3802@nyu.edu>
2024-03-21 11:40:02 -0400

    Implemented required form before using services

--- frontend/src/components/UserInfo.tsx
+++ frontend/src/components/UserInfo.tsx
@@ -1,108 +1,132 @@
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog"
-import {Input} from "@/components/ui/input"
-import {Label} from "@/components/ui/label"
-import {Button} from "@/components/ui/button";
-import cn from 'classnames'
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Button } from '@/components/ui/button';
+import cn from 'classnames';
 
-
-import React, {useState} from "react";
-import {useDispatch} from 'react-redux';
-import {setValidUser, validateUser} from '@/state/mainContent/mainContentSlice';
-import {AppDispatch} from '@/state/store';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 export const UserInfo = () => {
+  const [telegramInfo, setTelegramInfo] = useState({
+    token: '',
+    chat_id: '',
+  });
 
-        const tokenReceived = false;
+  // [open,setOpen] = useState(true);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setTelegramInfo({
+      ...telegramInfo,
+      [e.target.name]: value,
+    });
+  };
 
-        const [telegramInfo, setTelegramInfo] = useState({
-            token: '',
-            ChatID: ''
-        });
-        const dispatch = useDispatch<AppDispatch>();
-        const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-            const value = e.target.value;
-            setTelegramInfo({
-                ...telegramInfo,
-                [e.target.name]: value
-            });
-        };
+  // const fetchChatID = () => {
+  //     axios.get('http://127.0.0.1:3000/start')
+  //         .then(function (response) {
+  //             // handle success
+  //             console.log(response.data.ChatID);
+  //             // setTelegramInfo({
+  //             //     token: '',
+  //             //     ChatID: response.data.ChatID
+  //             // });
+  //         })
+  //
+  // };
 
-        const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-            e.preventDefault();
-            const formData = {
-                token: telegramInfo.token,
-                ChatID: telegramInfo.ChatID
-            };
-            dispatch(validateUser(formData));
-            dispatch(setValidUser());
-            setTelegramInfo({token: '', ChatID: ''});
+  return (
+    <Dialog defaultOpen={!telegramInfo.chat_id}>
+      <DialogContent
+        className="sm:max-w-[425px]"
+        onEscapeKeyDown={(e) => {
+          e.preventDefault();
+        }}
+        onPointerDownOutside={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <DialogHeader>
+          <DialogTitle>User Preferences ⚙️</DialogTitle>
+          <DialogDescription>
+            {telegramInfo.token
+              ? ''
+              : 'Input your token to continue'}
+          </DialogDescription>
+        </DialogHeader>
+        <form action="http://127.0.0.1:3000/secret" method="post">
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="font-bold text-right">
+                Token
+              </Label>
+              <Input
+                className="col-span-3"
+                id={'token'}
+                placeholder={'Please input your Bot Token'}
+                name={'token'}
+                type="text"
+                onChange={handleChange}
+                value={telegramInfo.token}
+              />
+            </div>
+            <div
+              className={cn(
+                telegramInfo.token
+                  ? 'grid grid-cols-4 items-center gap-4'
+                  : 'hidden'
+              )}
+            >
+              <Label htmlFor="username" className=" font-bold text-right">
+                ChatID
+              </Label>
+              <Input
+                className="col-span-3"
+                id={'chat_id'}
+                placeholder={'ChatID will show here'}
+                name={'chat_id'}
+                required={true}
+                type="text"
+                value={telegramInfo.chat_id || 'test'}
+              />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button
+              className={'text-white'}
+              type={'submit'}
+              onClick={(e) => {
+                e.preventDefault();
 
-        };
-
-        return (
-            <Dialog>
-                <DialogTrigger asChild>
-                    <Button className={'bg-blue-700'} variant="outline">User Preferences</Button>
-                </DialogTrigger>
-                <DialogContent className="sm:max-w-[425px]">
-                    <DialogHeader>
-                        <DialogTitle>User Preferences ⚙️</DialogTitle>
-                        <DialogDescription>
-                            {tokenReceived? 'Configure file saving location':'Input your token to continue'}
-                        </DialogDescription>
-                    </DialogHeader>
-                    <form onSubmit={handleSubmit}>
-                        <div className="grid gap-4 py-4">
-                            <div className="grid grid-cols-4 items-center gap-4">
-
-                                <Label htmlFor="name" className="font-bold text-right">
-                                    Token
-                                </Label>
-                                <Input
-                                    className="col-span-3"
-                                    id={'token'}
-                                    required={true}
-                                    placeholder={'Please input your Bot Token'}
-                                    name={'token'}
-                                    type="text"
-                                    onChange={handleChange}
-                                    value={telegramInfo.token}
-                                />
-                            </div>
-                            <div className={cn(tokenReceived? "grid grid-cols-4 items-center gap-4": "hidden")}>
-                                <Label htmlFor="username" className=" font-bold text-right">
-                                    ChatID
-                                </Label>
-                                <Input
-                                    className="col-span-3"
-                                    id={'ChatID'}
-                                    placeholder={'Please input your ChatID...'}
-                                    name={'ChatID'}
-                                    required={true}
-                                    type="text"
-                                    onChange={handleChange}
-                                    value={telegramInfo.ChatID}
-                                />
-
-                            </div>
-
-                        </div>
-                        <DialogFooter>
-                            <Button className={'text-white'} type="submit">Continue</Button>
-                        </DialogFooter>
-                    </form>
-
-                </DialogContent>
-            </Dialog>
-
-        )
-    }
-;
+                // fetchChatID();
+              }}
+            >
+              {' '}
+              Get ChatID
+            </Button>
+            <Button
+              className={'text-white'}
+              type="submit"
+              onClick={(e) => {
+                e.preventDefault();
+                const { token, chat_id } = telegramInfo;
+                const payfuckingload = { token, chat_id };
+                console.log(payfuckingload);
+                axios.post('http://localhost:3000/secret', payfuckingload);
+              }}
+            >
+              Save Changes{' '}
+            </Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
