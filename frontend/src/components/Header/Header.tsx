ee18f2766070f61f75343144fb0cfb03d342d857
sirichartchai <gs3802@nyu.edu>
2024-03-21 11:40:40 -0400

    Updated header design

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,13 +1,22 @@
+import cn from "classnames";
+
 const Header = () => {
+    // const recieved = false
+    const glass = "bg-white bg-opacity-20 shadow-xl backdrop-blur-md rounded-lg border border-white border-opacity-20 ";
 
     return (
         <>
-            <div className="text-center mt-14 mb-4">
-                <h1 className="font-semibold text-6xl flex justify-center mb-3">Tele <p className={'text-blue-500'}><u className={'underline underline-offset-8'}>Store</u></p></h1>
+            <div className="text-center mt-7 mb-4">
+                <h1 className={cn("text-6xl justify-center mb-3 text-2xl font-semibold flex")}>Tele <p
+                    className={'text-blue-500'}><u
+                    className={cn(  'underline underline-offset-3')}>Store</u>
+                </p></h1>
 
-                <p className="text-s opacity-50">
-                    Expand Your Reach, Not Your Costs - Agile Storage for All
+                <br/>
+                <p className={cn('text-s opacity-50')}>
+                    Encrypted cloud storage for all.
                 </p>
+
             </div>
         </>
     );
