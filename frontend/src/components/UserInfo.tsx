621574f47fd7e2d1a9fd4d6a9887ba37961bd0ff
sirichartchai <gs3802@nyu.edu>
2024-03-25 10:33:59 -0400

    Created interface to validate user

--- frontend/src/components/UserInfo.tsx
+++ frontend/src/components/UserInfo.tsx
@@ -5,6 +5,7 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
+  DialogClose
 } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
@@ -20,112 +21,119 @@ export const UserInfo = () => {
     chat_id: '',
   });
 
-  // [open,setOpen] = useState(true);
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
-    setTelegramInfo({
-      ...telegramInfo,
-      [e.target.name]: value,
-    });
+      setTelegramInfo((prevState) => ({
+        ...prevState,
+        token: value,
+      }));  };
+
+  const handleSave = () => {
+    const { token, chat_id } = telegramInfo;
+    const payload = { token, chat_id };
+    console.log(payload);
+    axios.post('http://localhost:3000/secret', payload);
   };
 
-  // const fetchChatID = () => {
-  //     axios.get('http://127.0.0.1:3000/start')
-  //         .then(function (response) {
-  //             // handle success
-  //             console.log(response.data.ChatID);
-  //             // setTelegramInfo({
-  //             //     token: '',
-  //             //     ChatID: response.data.ChatID
-  //             // });
-  //         })
-  //
-  // };
+  const fetchChatID = async () => {
+    try {
+      const status = await (
+        await axios.get('http://127.0.0.1:3000/start')
+      ).status;
+      console.log(status);
+    } catch (error) {
+      console.log(error);
+    }
+
+    try {
+      const res = await axios.get('http://127.0.0.1:3000/secret');
+      console.log(res);
+      setTelegramInfo((prevState) => ({
+        ...prevState,
+        chat_id: res.data.chat_id,
+      }));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <Dialog defaultOpen={!telegramInfo.chat_id}>
       <DialogContent
         className="sm:max-w-[425px]"
         onEscapeKeyDown={(e) => {
-          e.preventDefault();
+          !telegramInfo.chat_id ? e.preventDefault() : e;
         }}
         onPointerDownOutside={(e) => {
-          e.preventDefault();
+          !telegramInfo.chat_id ? e.preventDefault() : e;
         }}
       >
         <DialogHeader>
-          <DialogTitle>User Preferences ⚙️</DialogTitle>
+          <DialogTitle className="mb-5">User Preferences ⚙️</DialogTitle>
           <DialogDescription>
-            {telegramInfo.token
-              ? ''
-              : 'Input your token to continue'}
+            {telegramInfo.chat_id && telegramInfo.token
+              ? "You're all set!"
+              : telegramInfo.token
+              ? 'Once full token is submitted, please send "/start" to your bot'
+              : `Please input your bot's token to continue`}
           </DialogDescription>
         </DialogHeader>
-        <form action="http://127.0.0.1:3000/secret" method="post">
-          <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="name" className="font-bold text-right">
-                Token
-              </Label>
-              <Input
-                className="col-span-3"
-                id={'token'}
-                placeholder={'Please input your Bot Token'}
-                name={'token'}
-                type="text"
-                onChange={handleChange}
-                value={telegramInfo.token}
-              />
-            </div>
-            <div
-              className={cn(
-                telegramInfo.token
-                  ? 'grid grid-cols-4 items-center gap-4'
-                  : 'hidden'
-              )}
-            >
-              <Label htmlFor="username" className=" font-bold text-right">
-                ChatID
-              </Label>
-              <Input
-                className="col-span-3"
-                id={'chat_id'}
-                placeholder={'ChatID will show here'}
-                name={'chat_id'}
-                required={true}
-                type="text"
-                value={telegramInfo.chat_id || 'test'}
-              />
-            </div>
+        <div className="grid gap-4 py-4 pr-10">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="font-bold text-right">
+              Token
+            </Label>
+            <Input
+              className=" col-span-3"
+              id={'token'}
+              placeholder={'Please input your Bot Token'}
+              name={'token'}
+              type="text"
+              onChange={handleChange}
+              value={telegramInfo.token}
+            />
           </div>
-          <DialogFooter>
-            <Button
-              className={'text-white'}
-              type={'submit'}
-              onClick={(e) => {
-                e.preventDefault();
+          <div
+            className={cn(
+              telegramInfo.chat_id
+                ? 'grid grid-cols-4 items-center gap-4'
+                : 'hidden'
+            )}
+          >
+            <Label htmlFor="username" className=" font-bold text-right">
+              ChatID
+            </Label>
 
-                // fetchChatID();
-              }}
-            >
-              {' '}
-              Get ChatID
+            <Input
+              readOnly
+              className="col-span-3"
+              id={'chat_id'}
+              placeholder={'ChatID will show here'}
+              name={'chat_id'}
+              required={true}
+              type="text"
+              value={telegramInfo.chat_id}
+            />
+          </div>
+        </div>
+        <DialogFooter className=" w-10/12 mx-auto flex-col md:flex-end md:justify-end md:w-full">
+          <Button
+            className={'text-white mt-5 md:mt-0'}
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault();
+              handleSave();
+              fetchChatID();
+            }}
+          >
+            Continue
+          </Button>
+          <DialogClose asChild>
+            <Button className={cn(telegramInfo.chat_id? "block":"hidden")}type="button" variant="secondary">
+              Close
             </Button>
-            <Button
-              className={'text-white'}
-              type="submit"
-              onClick={(e) => {
-                e.preventDefault();
-                const { token, chat_id } = telegramInfo;
-                const payfuckingload = { token, chat_id };
-                console.log(payfuckingload);
-                axios.post('http://localhost:3000/secret', payfuckingload);
-              }}
-            >
-              Save Changes{' '}
-            </Button>
-          </DialogFooter>
-        </form>
+          </DialogClose>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
